58b6b661537ee9ec33148e963fba4d8bf11a495d
shivanshi <73399160+shivanshi-s@users.noreply.github.com>
2022-05-17 23:14:05 +0530

    Time: 15 ms (37.15%), Space: 15.3 MB (60.45%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -4,16 +4,20 @@ public:
         int n = pushed.size(); //size of the array
         stack<int> s; //declaring a stack
 //         loop for checking the stack
-        int i = 0 ;
+        int i = 0 ; // pointer for the popped 
         for( int j = 0 ; j < n; j++ )
         {
-            s.push(pushed[j]);
+            s.push(pushed[j]); //push current element onto the stack
+             // now see whether they required this element to pop, if yes,
+            // then pop and move pointer in our popped array 
+            // while the element matches with the top of the stack
             while(!s.empty() && s.top() == popped[i])
             {
                 s.pop();
                 i++;
             }
         }
+        // if stack is empty, then return true otherwise false
         return s.empty();
     }
 };
